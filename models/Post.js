f3c837e94e50bf9d590312b8f2211b87d93b71f2
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-20 15:37:52 -0800

    Add BlogPost model to handle blog posts

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const blogPostSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  contents: {
+    type: String,
+    required: true,
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = BlogPost;
