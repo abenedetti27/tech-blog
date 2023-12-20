565b2ac91ead192b9987575a2fa73c811fb3eef5
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-20 15:32:15 -0800

    Add Comment model definition

---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'BlogPost',
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
