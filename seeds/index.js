ebad88dfbee27ef5fa497389f344a57edb075b00
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-20 16:01:34 -0800

    Add seed data for MongoDB database

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const seedCommentData = require('./commentData');
+const seedPostData = require('./postData');
+const seedUserData = require('./userData');
+
+// Replace db url with the actual URL of MongoDB database
+mongoose.connect('your_database_url', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', async () => {
+  console.log('Connected to MongoDB');
+  await seedCommentData();
+  await seedPostData();
+  await seedUserData();
+  mongoose.connection.close();
+});
